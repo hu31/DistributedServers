d2a863294a8f6388f6cb108403fec4a3d66b86d2
Tian Lin Hu <tianlin.who@gmail.com>
2018-12-01 09:27:54 +0800

    Init

---
+++ src/server/main/Server.java
@@ -0,0 +1,22 @@
+package server.main;
+
+public class Server {
+
+	private Server() {
+		
+	}
+	
+	private Server(int port) {
+		
+	}
+	
+	public static Server createServer() {
+		return new Server();
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/utility/Comm.java
@@ -0,0 +1,8 @@
+package utility;
+
+public interface Comm {
+
+	public String get(String key);
+	
+	public String put(String key, String value);
+}
